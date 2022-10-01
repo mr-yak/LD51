f0c7a6793b9911393ca77373d1bd9ea9cf861ba4
mr-yak <41585@kristin.school.nz>
2022-10-01 15:49:32 +1300

    Update TileMap.gd

--- TileMap.gd
+++ TileMap.gd
@@ -1,15 +1,16 @@
 extends TileMap
 
 var cells
-onready var timers_node = get_node(../Main)
+onready var timers_node = get_tree().get_root().get_node("Main/Timers")
 
 func _ready():
 	cells = get_used_cells()
 	var timers = [cells.size()]
 	for cell in cells:
 		var index = cells.find(cell)
-		timers[index] = Timer.new()
-		#$Main/Timers.add_child(timers[index])
+		print(cells.size())
+		#timers[index] = Timer.new()
+		timers_node.add_child(timers[index])
 
 func _unhandled_input(event):
 	if event.is_pressed():
