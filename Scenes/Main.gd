cff2ea483767c365d9abc003046e16d0e82540b6
Jel2217 <54785779+Jel2217@users.noreply.github.com>
2022-10-01 16:50:56 +1200

    ars

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var coins = 0;
 var item1 = load("res://seeds/seed.png")
+onready var mouse = get_tree().get_root().get_node("Main/UI/MouseFollow")
 onready var inventory = $UI/ItemList
 
 func _ready():
@@ -13,5 +14,9 @@ func _process(delta):
 
 func _on_ItemList_item_selected(index):
 	inventory.remove_item(index)
-	
-	pass # Replace with function body.
+	mouse.texture = item1
+
+
+func _on_ItemList_nothing_selected():
+	inventory.add_icon_item(mouse.texture)
+	mouse.texture = null
