4929b432ecd31043cbeb7034b1d487eff15d3dfa
mr-yak <41585@kristin.school.nz>
2022-10-01 14:51:52 +1300

    abort
    abort

--- CharachterController.gd
+++ CharachterController.gd
@@ -1,8 +1,33 @@
 extends RigidBody
 
-var power = 10
-func _physics_process(delta):
-	if Input.is_action_pressed("jump"):
-		if(get_colliding_bodies() != []):
-			add_central_impulse(Vector3.UP*power)
-		
+var power = 4
+var velocity = Vector3.ZERO
+
+func _ready():
+	add_central_force(Vector3(0,-9.81,0))
+
+func _process(_delta):
+	velocity = linear_velocity
+	if Input.is_action_just_pressed("forward"):
+		velocity += Vector3.FORWARD
+	elif Input.is_action_just_released("forward"):
+		velocity -= Vector3.FORWARD
+	if Input.is_action_just_pressed("back"):
+		velocity += Vector3.BACK
+	elif Input.is_action_just_released("back"):
+		velocity -= Vector3.BACK
+	if Input.is_action_just_pressed("left"):
+		velocity += Vector3.LEFT
+	elif Input.is_action_just_released("left"):
+		velocity -= Vector3.LEFT
+	if Input.is_action_just_pressed("right"):
+		velocity += Vector3.RIGHT
+	elif Input.is_action_just_released("right"):
+		velocity -= Vector3.RIGHT
+
+func _physics_process(_delta):
+
+	if Input.is_action_just_pressed("jump") && get_colliding_bodies().size()>=1:
+		apply_central_impulse(Vector3.UP*power)
+	linear_velocity = velocity
+	
