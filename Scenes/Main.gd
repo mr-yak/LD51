c54d1e45ec3c0d2c14ccd73b0b6756bc45aab824
Jel2217 <54785779+Jel2217@users.noreply.github.com>
2022-10-01 16:41:32 +1200

    stuf

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -1,6 +1,17 @@
 extends Node2D
 
 var coins = 0;
+var item1 = load("res://seeds/seed.png")
+onready var inventory = $UI/ItemList
+
+func _ready():
+	inventory.add_icon_item(item1)
 
 func _process(delta):
 	$UI/Coin_Count.text = String(coins)
+
+
+func _on_ItemList_item_selected(index):
+	inventory.remove_item(index)
+	
+	pass # Replace with function body.
