660e53ec3cf83716ce4f703fcbfe24c7f2bbe7c0
Jel2217 <54785779+Jel2217@users.noreply.github.com>
2022-10-02 14:09:50 +1200

    anim

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -38,8 +38,8 @@ func _process(delta):
 		var pos = tiles.world_to_map(tiles.get_local_mouse_position())
 		if cells.has(pos):
 			var cell_type = tiles.get_cellv(pos)
-			if cell_type == 0 and planting_mode and mouse.seed_holding != 0:
-				plant_crop(pos, mouse.seed_holding)
+			if cell_type == 1 and planting_mode and mouse.seed_holding != 0:
+				plant_crop(pos, mouse.seed_holding - 1)
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		var pos = tiles.world_to_map(tiles.get_local_mouse_position())
 		if cells.has(pos):
@@ -53,10 +53,6 @@ func save_crop(pos,crop_type):
 func get_crop_type(pos):
 	return crops[pos.x][pos.y]
 
-func _on_timer_timeout(pos, timer_node):
-	timer_node.queue_free()
-	tiles.set_cellv(pos, 2)
-
 func _unhandled_input(event):
 	if event.is_pressed():
 		if mouse.texture != null and event.is_action("click"):
@@ -66,19 +62,34 @@ func _unhandled_input(event):
 			var cell_type = tiles.get_cellv(pos)
 			if cell_type == 2: 
 				planting_mode = false
-			elif cell_type == 0:
+			elif cell_type == 1:
 				planting_mode = true
 
 func plant_crop(crop_pos, crop_type):
 	if(coins - planting_costs[crop_type]>=0):
 		coins -= planting_costs[crop_type]
-		tiles.set_cellv(crop_pos, 1)
+		tiles.set_cellv(crop_pos, seeds.tile_index_seed[crop_type])
 		save_crop(crop_pos, crop_type)
 		var timer = Timer.new()
-		timer.connect("timeout",self,"_on_timer_timeout", [crop_pos, timer]) 
-		timer.wait_time = 10.0
+		timer.connect("timeout",self,"_on_timer_1_timeout", [crop_pos, timer, 2.0, crop_type]) 
+		timer.wait_time = 2.0
 		timer.autostart = true
 		timers_node.add_child(timer)
+
+func _on_timer_1_timeout(pos, timer_node, time, crop_type):
+	print("timer 1 timeout")
+	timer_node.queue_free()
+	tiles.set_cellv(pos, seeds.tile_index_growing[crop_type])
+	var timer = Timer.new()
+	timer.connect("timeout",self,"_on_timer_2_timeout", [pos, timer, crop_type]) 
+	timer.wait_time = time
+	timer.autostart = true
+	timers_node.add_child(timer)
+
+func _on_timer_2_timeout(pos, timer_node, crop_type):
+	print("timer 2 timeout")
+	tiles.set_cellv(pos, seeds.tile_index_grown[crop_type])
+	timer_node.queue_free()
 
 func collect_crop(crop_pos):
 	coins += crop_value[get_crop_type(crop_pos)]
