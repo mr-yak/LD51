e7dc2386895bb222602a5535c4a5dbca3d75c23c
mr-yak <41585@kristin.school.nz>
2022-10-02 13:10:26 +1300

    Capatalism

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-var coins = 0;
+var planting_costs = [100, 200] 
+var crop_value = [200, 500]
+var unlock_cost = [0, 1500]
+var crops = []
+
+
+var coins = 1000;
 var item1 = load("res://seeds/seed.png")
 var seeds = load("res://Resources/seeds.tres")
 onready var mouse = get_tree().get_root().get_node("Main/UI/MouseFollow")
@@ -12,12 +18,20 @@ var planting_mode = false
 var cells
 onready var timers_node = get_tree().get_root().get_node("Main/Timers")
 
+
+
 func _ready():
+	crop_array_init()
 	cells = tiles.get_used_cells()
 	print(seeds.name[0])
 
+func crop_array_init():
+	for x in range(7):
+		crops.append([])
+		for y in range(6):
+			crops[x].append(0)
 func _process(delta):
-	$UI/Coin_Count.text = String(coins)
+	$UI/Coin_Count.text = "$" + String(coins)
 	
 	if Input.is_mouse_button_pressed(BUTTON_RIGHT):
 		var pos = tiles.world_to_map(tiles.get_local_mouse_position())
@@ -31,6 +45,12 @@ func _process(delta):
 			var cell_type = tiles.get_cellv(pos)
 			if cell_type == 2 and planting_mode == false:
 				collect_crop(pos)
+
+func save_crop(pos,crop_type):
+	crops[pos.x][pos.y] = crop_type
+
+func get_crop_type(pos):
+	return crops[pos.x][pos.y]
 
 func _on_timer_timeout(pos, timer_node):
 	timer_node.queue_free()
@@ -49,20 +69,30 @@ func _unhandled_input(event):
 				planting_mode = true
 
 func plant_crop(crop_pos, crop_type):
-	tiles.set_cellv(crop_pos, 1)
-	var timer = Timer.new()
-	timer.connect("timeout",self,"_on_timer_timeout", [crop_pos, timer]) 
-	timer.wait_time = 10.0
-	timer.autostart = true
-	timers_node.add_child(timer)
+	if(coins - planting_costs[crop_type]>=0):
+		coins -= planting_costs[crop_type]
+		tiles.set_cellv(crop_pos, 1)
+		save_crop(crop_pos, crop_type)
+		var timer = Timer.new()
+		timer.connect("timeout",self,"_on_timer_timeout", [crop_pos, timer]) 
+		timer.wait_time = 10.0
+		timer.autostart = true
+		timers_node.add_child(timer)
+	else:
+		pass
 
 func collect_crop(crop_pos):
+	coins += crop_value[get_crop_type(crop_pos)]
 	tiles.set_cellv(crop_pos, 0)
-	coins += 1
 
 func add_to_inventory(seed_num):
-	inventory.add_icon_item(seeds.texture[seed_num])
-	items_in_list.append(seeds.texture[seed_num])
+	if(coins - unlock_cost[seed_num]>=0):
+		$UI/Shop.set_item_disabled(seed_num, true)
+		inventory.add_icon_item(seeds.texture[seed_num])
+		items_in_list.append(seeds.texture[seed_num])
+		coins -= unlock_cost[seed_num]
+	else:
+		pass
 
 func _on_ItemList_item_selected(index):
 	if mouse.texture != null:
--- Scripts/Shop.gd
+++ Scripts/Shop.gd
@@ -1,14 +1,18 @@
 extends ItemList
 var seeds = load("res://Resources/seeds.tres")
-
+var bought = []
+var seed_number = 2
 func _ready():
-	for i in [0,1]:
+	for i in range(seed_number):
+		bought.append(false)
 		add_icon_item(seeds.texture[i])
 		
 
 
 func _on_Shop_item_selected(index):
 	unselect(index)
-	set_item_disabled(index, true)
-	get_tree().get_root().get_node("Main").add_to_inventory(index)
-	
+	if(!bought[index]):
+		bought[index] = true
+		get_tree().get_root().get_node("Main").add_to_inventory(index)
+	else:
+		pass
