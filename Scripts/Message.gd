c2b78b0f68b4805cfdda764dee0ededa4735cf0f
mr-yak <41585@kristin.school.nz>
2022-10-02 18:27:06 +1300

    Added more death messages

--- Scripts/Message.gd
+++ Scripts/Message.gd
@@ -1,6 +1,11 @@
 extends Label
 
 var messages = ["Killed", "Hung", "Executed", "Thrown in Prison", "Eaten By Lions", "Left to Die"]
+var rng = RandomNumberGenerator.new()
 func _ready():
-	var key = rand_range(0, messages.size()-1)
+	rng.set_seed(7340651920651)
+	for i in range(10):
+		randomize()
+		rng.randomize()
+	var key = rng.randi_range(0, messages.size()-1)
 	text = "You Got " + messages[key]
