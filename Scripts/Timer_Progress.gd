9c6f65374824d236a56a524a444cd0cf201446f5
mr-yak <41585@kristin.school.nz>
2022-10-03 13:56:23 +1300

    Reworked Health bar

--- Scripts/Timer_Progress.gd
+++ Scripts/Timer_Progress.gd
@@ -1,11 +1,13 @@
-extends ProgressBar
+extends TextureProgress
 
 var rnd = 0
 onready var req_coin_label = get_tree().get_root().get_node("Main/UI/Required_Coins")
 
 func _process(_delta):
-	value = $Quota_Timer.time_left
-
+	if value != ceil($Quota_Timer.time_left):
+		value = ceil($Quota_Timer.time_left)
+		get_tree().get_root().get_node("Main/Sounds/TickSFX").play()
+		
 func _on_Quota_Timer_timeout():
 	rnd += 1
 	req_coin_label.update_timer(rnd)
